9044cce9ff3b36a39eae1e9e1596e56025376223
pangpangE123 <eric0420@naver.com>
2023-10-12 23:10:41 +0900

    소켓아웃노드 추가

---
+++ xflow-project/src/main/java/com/nhnacademy/node/SocketOutNode.java
@@ -0,0 +1,53 @@
+package com.nhnacademy.node;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import com.nhnacademy.exception.InvalidArgumentException;
+import com.nhnacademy.exception.OutOfBoundsException;
+import com.nhnacademy.message.Message;
+import com.nhnacademy.port.Port;
+
+public class SocketOutNode extends OutputNode {
+
+    Socket socket;
+
+
+
+    public SocketOutNode(Socket socket) {
+        super(count);
+        this.socket = socket;
+    }
+
+
+
+    @Override
+    public void process() {
+
+        try {
+            BufferedWriter socketOut =
+                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            for (int i = 0; i < getInputCount(); i++) {
+                if (getInput(i).hasMessage()) {
+                    Message message = getInput(i).get();
+                    // 객체를 String으로 형변환을 해줘야함
+                    socketOut.write(message.toString());
+                    socketOut.flush();
+                }
+
+            }
+
+
+
+        } catch (IOException e) {
+            //
+
+        }
+
+    }
+
+}
