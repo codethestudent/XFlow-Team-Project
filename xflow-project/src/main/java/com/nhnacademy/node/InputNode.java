36a7753aad852db93e17a74608de22cbb3b8903c
{qaw302} <{osshun21@gmail.com}>
2023-10-13 17:19:50 +0900

    tracewire 추가, tracewire에 put 하는 동작추가

--- xflow-project/src/main/java/com/nhnacademy/node/InputNode.java
+++ xflow-project/src/main/java/com/nhnacademy/node/InputNode.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public abstract class InputNode extends ActiveNode {
     Wire[] outputWires;
+    Wire traceWire;
 
     InputNode(String name, int count) {
         super(name);
@@ -61,6 +62,9 @@ public abstract class InputNode extends ActiveNode {
         for (Wire wire : outputWires) {
             if (wire != null) {
                 wire.put(message);
+                if (traceWire != null) {
+                    traceWire.put(message);
+                }
             }
         }
     }
