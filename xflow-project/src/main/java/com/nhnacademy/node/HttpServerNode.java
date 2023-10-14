72c77e6ba53c2396c480af9a56dae7562662b21b
codeTheStudent <hjun303@gnu.ac.kr>
2023-10-14 17:18:18 +0900

    서버 노드 생성

---
+++ xflow-project/src/main/java/com/nhnacademy/node/HttpServerNode.java
@@ -0,0 +1,12 @@
+package com.nhnacademy.node;
+
+/*
+ * 음,,, 일단 여기선 서버를 열긴 해야하는데 이제,,, json 파일도 ems로 부터 요청을 해야해서
+ * 음,,,,,,,,, 일단 소켓 승인으로 여는거 부터 하자.
+ */
+
+public class HttpServerNode extends InputOutputNode {
+    HttpServerNode() {
+        super(1, 1);
+    }
+}
